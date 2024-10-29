9f47ef444a9108045ce9aa1d3af80ba1aa4b0934
Sarah <125749615+sarahtoninwrites@users.noreply.github.com>
2024-10-29 12:33:50 +0530

    adding works

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById('flipper1').addEventListener('click', function() {
+  this.classList.toggle('flip');
+});
+document.getElementById('flipper2').addEventListener('click', function() {
+this.classList.toggle('flip');
+});
