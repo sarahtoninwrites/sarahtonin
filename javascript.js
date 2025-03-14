74d31cf0a6425ecd632dee705a0b90a086980bae
Sarah <125749615+sarahtoninwrites@users.noreply.github.com>
2025-03-14 13:53:56 +0530

    updating whole ting

--- javascript.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('flipper1').addEventListener('click', function() {
-  this.classList.toggle('flip');
-});
-document.getElementById('flipper2').addEventListener('click', function() {
-this.classList.toggle('flip');
-});
